82626439427535ce79c5c8e64941652d4b21715b
Ramiro <ramiro.al.alvarez@gmail.com>
2020-11-06 17:09:57 -0300

    version 4- responsive design

--- js/app.js
+++ js/app.js
@@ -67,9 +67,15 @@ const about = document.querySelector('#about');
 const skills = document.querySelector('#skills');
 const portfolio = document.querySelector('#portfolio');
 //const nav = document.querySelector('.main-menu');
+let x = window.matchMedia("(min-width: 768px)")
+
 foto.addEventListener('click', () =>{
 
+
+  if(x.matches){
   foto.setAttribute('id', 'foto-gallery')
+  }
+  
   background.style.backgroundColor = ' rgba(0, 0, 0, 0.055)';
   about.style.color ='#00065F';   
   skills.style.color ='#00065F';   
